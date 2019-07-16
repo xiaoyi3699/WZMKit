7da3f5cbe2c50873d3426ec4bf983a1e63afdeb9
wang <122589615@qq.com>
2019-07-16 20:57:14 +0800

    init

---
+++ WZMKit/Classes/WZMTools/WZMUncaughtException/WZMSignalException.h
@@ -0,0 +1,15 @@
+//
+//  WZMSignalException.h
+//  WZMKit
+//
+//  Created by WangZhaomeng on 2018/2/11.
+//  Copyright © 2018年 WangZhaomeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WZMSignalException : NSObject
+
+void WZMInstallSignalHandler(void);
+
+@end
