96a9bb28e9196209a88e2c7509a5c35ee60f2ff2
wang <122589615@qq.com>
2019-07-17 21:06:23 +0800

    init

---
+++ Example/WZMKit/DemoClass/FirstViewController.m
@@ -0,0 +1,31 @@
+//
+//  FirstViewController.m
+//  WZMKit
+//
+//  Created by wangzhaomeng on 07/15/2019.
+//  Copyright (c) 2019 wangzhaomeng. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.title = @"第一页";
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+}
+
+@end
