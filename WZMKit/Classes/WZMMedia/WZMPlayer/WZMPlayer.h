edf68db46f2d3dfa1e93af3e41c9c6034ded4085
wang <122589615@qq.com>
2019-07-17 15:21:31 +0800

    init

--- WZMKit/Classes/WZMMedia/WZMPlayer/WZMPlayer.h
+++ WZMKit/Classes/WZMMedia/WZMPlayer/WZMPlayer.h
@@ -4,7 +4,7 @@
 //
 //  Created by WangZhaomeng on 2017/4/16.
 //  Copyright © 2017年 MaoChao Network Co. Ltd. All rights reserved.
-//
+//  音频/视频播放器
 
 #import <Foundation/Foundation.h>
 #import "WZMPlayerView.h"
@@ -12,14 +12,21 @@
 
 @interface WZMPlayer : NSObject
 
-@property (nonatomic, readonly, assign) CGFloat   playProgress; //播放进度
-@property (nonatomic, readonly, assign) CGFloat   loadProgress; //缓冲进度
-@property (nonatomic, readonly, assign) NSInteger duration;     //音频总时长
-@property (nonatomic, readonly, assign) NSInteger currentTime;  //当前播放时间
-
+///播放进度
+@property (nonatomic, readonly, assign) CGFloat   playProgress;
+///缓冲进度
+@property (nonatomic, readonly, assign) CGFloat   loadProgress;
+///音频总时长
+@property (nonatomic, readonly, assign) NSInteger duration;
+///当前播放时间
+@property (nonatomic, readonly, assign) NSInteger currentTime;
+///播放状态
 @property (nonatomic, assign, getter=isPlaying) BOOL playing;
+///是否允许后台播放, 默认NO, 后台播放需要应用简单配置
 @property (nonatomic, assign, getter=isBackground) BOOL background;
+///代理事件
 @property (nonatomic, weak) id<WZMPlayerDelegate> delegate;
+///播放音频时不用设置, 播放视频是需要设置视图
 @property (nonatomic, strong) WZMPlayerView *playerView;
 
 - (void)playWithURL:(NSURL *)url;
--- WZMKit/Classes/WZMMedia/WZMPlayer/WZMPlayerView.h
+++ WZMKit/Classes/WZMMedia/WZMPlayer/WZMPlayerView.h
@@ -3,7 +3,7 @@
 //  Pods-WZMKit_Example
 //
 //  Created by WangZhaomeng on 2019/7/17.
-//
+//  视频播放视图
 
 #import <UIKit/UIKit.h>
 
