14fda34c080eb04d8891c6fa6efc63a9efb4dce8
wang <122589615@qq.com>
2019-07-22 12:02:15 +0800

    init

---
+++ Example/WZMKit/DemoClass/WZMInputView/WZMInputView/Keyboard/WZMEmojisKeyboard/View/RecordAnimation/WZMRecordAnimation.h
@@ -0,0 +1,20 @@
+//
+//  WZMRecordAnimation.h
+//  WZMChat
+//
+//  Created by WangZhaomeng on 2019/5/23.
+//  Copyright © 2019 WangZhaomeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WZMRecordAnimation : UIImageView
+
+///设置声音大小 (0 ~ 1)
+@property (nonatomic, assign) CGFloat volume;
+
+- (void)showVoiceCancel;
+- (void)showVoiceShort;
+- (void)showVoiceAnimation;
+
+@end
