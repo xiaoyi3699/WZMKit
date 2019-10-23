62ada9812d3539a7d37e3c3ddaa1bb2b0d68f1d5
wang <122589615@qq.com>
2019-10-23 15:47:39 +0800

    init

--- Example/WZMKit/DemoClass/MMVC/First/Controller/FirstViewController.m
+++ Example/WZMKit/DemoClass/MMVC/First/Controller/FirstViewController.m
@@ -28,6 +28,8 @@
 
 #import "FirstViewController.h"
 #import "WZMNewsDataProvider.h"
+#import <CommonCrypto/CommonCrypto.h>
+#import <CommonCrypto/CommonDigest.h>
 
 @interface FirstViewController ()<UITableViewDelegate,UITableViewDataSource> {
     UITableView *_tableView;
@@ -61,12 +63,16 @@
 #pragma mark - UITableViewDelegate,UITableViewDataSource
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if (indexPath.row < _newDataProvider.currentList.count) {
-        WZMNewsModel *model = _newDataProvider.currentList[indexPath.row];
-        WZMWebViewController *webVC = [[WZMWebViewController alloc] initWithUrl:model.url];
-        webVC.hidesBottomBarWhenPushed = YES;
-        [self.navigationController pushViewController:webVC animated:YES];
-    }
+//    if (indexPath.row < _newDataProvider.currentList.count) {
+//        WZMNewsModel *model = _newDataProvider.currentList[indexPath.row];
+//        WZMWebViewController *webVC = [[WZMWebViewController alloc] initWithUrl:model.url];
+//        webVC.hidesBottomBarWhenPushed = YES;
+//        [self.navigationController pushViewController:webVC animated:YES];
+//    }
+    
+    NSString *m = [self AES128Encrypt:@"abcdefghijk" key:@"wang"];
+    NSString *j = [self AES128Decrypt:m key:@"wang"];
+    NSLog(@"%@===%@",m,j);
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -87,5 +93,71 @@
 }
 
 //基类的一些常用函数
+//AES128位加密 base64编码 注：kCCKeySizeAES128点进去可以更换256位加密
+-(NSString *)AES128Encrypt:(NSString *)plainText key:(NSString *)key {
+    char keyPtr[kCCKeySizeAES128+1];//
+    memset(keyPtr, 0, sizeof(keyPtr));
+    [key getCString:keyPtr maxLength:sizeof(keyPtr) encoding:NSUTF8StringEncoding];
+    
+    NSData* data = [plainText dataUsingEncoding:NSUTF8StringEncoding];
+    NSUInteger dataLength = [data length];
+    
+    size_t bufferSize = dataLength + kCCBlockSizeAES128;
+    void *buffer = malloc(bufferSize);
+    size_t numBytesEncrypted = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCEncrypt,
+                                          kCCAlgorithmAES,
+                                          kCCOptionPKCS7Padding|kCCOptionECBMode,
+                                          keyPtr,
+                                          kCCBlockSizeAES128,
+                                          NULL,
+                                          [data bytes],
+                                          dataLength,
+                                          buffer,
+                                          bufferSize,
+                                          &numBytesEncrypted);
+    if (cryptStatus == kCCSuccess) {
+        NSData *resultData = [NSData dataWithBytesNoCopy:buffer length:numBytesEncrypted];
+        
+        NSString *stringBase64 = [resultData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed]; // base64格式的字符串
+        return stringBase64;
+        
+    }
+    free(buffer);
+    return nil;
+}
+
+//解密
+-(NSString *)AES128Decrypt:(NSString *)encryptText key:(NSString *)key {
+    char keyPtr[kCCKeySizeAES128 + 1];
+    memset(keyPtr, 0, sizeof(keyPtr));
+    [key getCString:keyPtr maxLength:sizeof(keyPtr) encoding:NSUTF8StringEncoding];
+    
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:encryptText options:NSDataBase64DecodingIgnoreUnknownCharacters];//base64解码
+    
+    NSUInteger dataLength = [data length];
+    size_t bufferSize = dataLength + kCCBlockSizeAES128;
+    void *buffer = malloc(bufferSize);
+    
+    size_t numBytesCrypted = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt,
+                                          kCCAlgorithmAES,
+                                          kCCOptionPKCS7Padding|kCCOptionECBMode,
+                                          keyPtr,
+                                          kCCBlockSizeAES128,
+                                          NULL,
+                                          [data bytes],
+                                          dataLength,
+                                          buffer,
+                                          bufferSize,
+                                          &numBytesCrypted);
+    if (cryptStatus == kCCSuccess) {
+        NSData *resultData = [NSData dataWithBytesNoCopy:buffer length:numBytesCrypted];
+        
+        return [[NSString alloc] initWithData:resultData encoding:NSUTF8StringEncoding];
+    }
+    free(buffer);
+    return nil;
+}
 
 @end
