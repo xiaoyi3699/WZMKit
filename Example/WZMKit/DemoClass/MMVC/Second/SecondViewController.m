1e78c2ee2a2b15c6ca22d2b536b19680e00ffff7
wangzhaomeng <122589615@qq.com>
2020-05-09 10:04:06 +0800

    细节修改

--- Example/WZMKit/DemoClass/MMVC/Second/SecondViewController.m
+++ Example/WZMKit/DemoClass/MMVC/Second/SecondViewController.m
@@ -8,6 +8,7 @@
 
 #import "SecondViewController.h"
 #import "WZMIAPManager.h"
+#import "WZMMenuView.h"
 //http://www.vasueyun.cn/resource/wzm_snow.mp3
 //http://www.vasueyun.cn/resource/wzm_qnyh.mp4
 
@@ -28,7 +29,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
+    WZMMenuView *menuView = [[WZMMenuView alloc] initWithFrame:CGRectMake(0.0, 100.0, WZM_SCREEN_WIDTH, 300.0)];
+    menuView.backgroundColor = [UIColor redColor];
+    menuView.titles = @[@"阿迪王大无",@"带娃",@"带娃大无大无大",@"吊袜带哇",@"带娃大无",@"",@"带娃",@"打完大无哇无",@"我打到无",@"打到我",@"吊袜带哇奥无",@"大娃娃大哇多哇",];
+    [self.view addSubview:menuView];
 }
 
 @end
---
+++ Example/WZMKit/DemoClass/MMVC/Second/WZMMenuView.h
@@ -0,0 +1,19 @@
+//
+//  WZMMenuView.h
+//  WZMKit_Example
+//
+//  Created by Zhaomeng Wang on 2020/5/9.
+//  Copyright © 2020 wangzhaomeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WZMMenuView : UIView
+
+@property (nonatomic, strong) NSArray *titles;
+
+@end
+
+NS_ASSUME_NONNULL_END
