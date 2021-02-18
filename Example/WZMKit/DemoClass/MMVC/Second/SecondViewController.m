3de7cd7200452b4dd0cced6e5a006cec665c7c86
wangzhaomeng <122589615@qq.com>
2021-02-18 09:19:55 +0800

    init

--- Example/WZMKit/DemoClass/MMVC/Second/SecondViewController.m
+++ Example/WZMKit/DemoClass/MMVC/Second/SecondViewController.m
@@ -29,18 +29,10 @@
     
     self.view.backgroundColor = [UIColor grayColor];
     
-    WZMDrawView *v = [[WZMDrawView alloc] initWithFrame:WZMRectMiddleArea()];
-    v.color = [UIColor redColor];
-    v.image = [UIImage imageNamed:@"meinv"];
-    v.contentMode = UIViewContentModeScaleAspectFill;
-    [self.view addSubview:v];
-    _v = v;
-    
-    return;
     UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(100.0, 100.0, 50.0, 50.0)];
     btn.titleLabel.font = [UIFont systemFontOfSize:15];
     btn.backgroundColor = [UIColor orangeColor];
-    [btn setTitle:@"橡皮" forState:UIControlStateNormal];
+    [btn setTitle:@"111" forState:UIControlStateNormal];
     [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
@@ -49,58 +41,18 @@
     btn2.tag = 1;
     btn2.titleLabel.font = [UIFont systemFontOfSize:15];
     btn2.backgroundColor = [UIColor orangeColor];
-    [btn2 setTitle:@"撤销" forState:UIControlStateNormal];
+    [btn2 setTitle:@"222" forState:UIControlStateNormal];
     [btn2 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [btn2 addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn2];
-    
-    UIImage *image = [UIImage imageNamed:@"meinv"];
-    CGSize size = CGSizeMake(108.0, 144.0);
-    {
-        //生成一张
-        UIImage *hbImage = [UIImage wzm_getRectImageByColor:[UIColor whiteColor] size:size];
-        UIGraphicsBeginImageContextWithOptions(size, NO, [[UIScreen mainScreen] scale]);
-        [hbImage drawInRect:CGRectMake(0, 0 , size.width, size.height)];
-        [image drawInRect:CGRectMake(0.0, 0.0, size.width, size.height)];
-        UIImage *newPic = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        [WZMAlbumHelper wzm_saveImage:newPic completion:^(NSError *error) {
-            NSLog(@"11111");
-        }];
-    }
-    
-    {
-        //生成6张
-        NSInteger c = 3, r = 3;
-        CGFloat s = 5.0;
-        CGFloat w = s*(c+1)+size.width*c;
-        CGFloat h = s*(r+1)+size.height*r;
-        
-        CGSize hbSize = CGSizeMake(w, h);
-        UIImage *hbImage = [UIImage wzm_getRectImageByColor:[UIColor whiteColor] size:hbSize];
-        UIGraphicsBeginImageContextWithOptions(hbSize, NO, [[UIScreen mainScreen] scale]);
-        [hbImage drawInRect:CGRectMake(0, 0 , hbSize.width, hbSize.height)];
-        for (NSInteger i = 0; i < (c*r); i ++) {
-            CGRect rect = CGRectMake(s+i%c*(size.width+s), s+i/c*(size.height+s), size.width, size.height);
-            [image drawInRect:rect];
-        }
-        UIImage *newPic = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        [WZMAlbumHelper wzm_saveImage:newPic completion:^(NSError *error) {
-            NSLog(@"11111");
-        }];
-    }
 }
 
 - (void)btnClick:(UIButton *)btn {
     if (btn.tag == 0) {
-        _v.lineWidth = arc4random()%10 + 5;
-        _v.eraser = !_v.isEraser;
+        
     }
     else {
-        [_v backforward];
+        
     }
 }
 
