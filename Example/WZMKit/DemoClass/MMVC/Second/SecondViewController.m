ddd4873ec3ccc7ba3896dc9998e9e1768a584aa2
wangzhaomeng <122589615@qq.com>
2021-02-20 17:21:11 +0800

    init

--- Example/WZMKit/DemoClass/MMVC/Second/SecondViewController.m
+++ Example/WZMKit/DemoClass/MMVC/Second/SecondViewController.m
@@ -12,9 +12,7 @@
 
 @end
 
-@implementation SecondViewController {
-    WZMDrawView *_v;
-}
+@implementation SecondViewController
 
 - (instancetype)init {
     self = [super init];
@@ -24,77 +22,19 @@
     return self;
 }
 
-- (void)rot {
-    
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIView *testView = [[UIView alloc] initWithFrame:CGRectMake(100.0, 200.0, 200.0, 200.0)];
-    testView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:testView];
-    
-    WZMSingleRotationGestureRecognizer *ges = [[WZMSingleRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rot)];
-    ges.activeRect = CGRectMake(160.0, 160.0, 40.0, 40.0);
-    [testView addGestureRecognizer:ges];
-    
-//    WZMDrawView *drawView = [[WZMDrawView alloc] initWithFrame:self.contentView.bounds];
-//    drawView.contentMode = UIViewContentModeScaleAspectFill;
-//    drawView.hbImages = @[[UIImage imageNamed:@"tabbar_icon_on"],@"tabbar_icon"];
-//    [self.contentView addSubview:drawView];
-    
-    return;
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10.0, 100.0, 355.0, 200.0)];
-    imageView.image = [UIImage imageNamed:@"bgcolors"];
-    [self.view addSubview:imageView];
-    
-    UILabel *label = [[UILabel alloc] initWithFrame:imageView.bounds];
-    label.text = @"d神打哇多哇多哇多无dawa达瓦多哇dwadada打到我w🙂weaeawe 带娃大无";
-    label.numberOfLines = 0;
-//    label.backgroundColor = [UIColor redColor];
-    
-//    imageView.wzm_hollow = YES;
-    imageView.wzm_maskView = label;
-    
-//    UIImageView *imageView2 = [[UIImageView alloc] initWithFrame:imageView.bounds];
-//    imageView2.image = [UIImage imageNamed:@"pikaqiu"];
-//
-//    imageView.wzm_hollow = YES;
-//    imageView.wzm_maskView = imageView2;
-    
-    return;
-    CALayer *imageLayer = [[CALayer alloc] init];
-    imageLayer.frame = CGRectMake(10.0, 100.0, 255.0, 50.0);
-    imageLayer.contents = (__bridge id)([UIImage imageNamed:@"meinv"].CGImage);
-    [self.view.layer addSublayer:imageLayer];
-    
-    NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
-    style.lineSpacing = 5.0;
-    style.alignment = NSTextAlignmentCenter;
-    style.lineBreakMode = NSLineBreakByCharWrapping;
-    
-    NSString *text = @"床前明月光";
-    NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:text];
-    [attStr addAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:30.0], NSParagraphStyleAttributeName:style} range:NSMakeRange(0, attStr.length)];
-    
-    UIBezierPath *path = [attStr wzm_getBezierPath];
-    CAShapeLayer *shaperLayer = [[CAShapeLayer alloc] init];
-    shaperLayer.path = path.CGPath;
-    shaperLayer.frame = imageLayer.bounds;
-    shaperLayer.fillRule = kCAFillRuleEvenOdd;
-    
-    imageLayer.mask = shaperLayer;
+}
+
+- (WZMContentType)contentType {
+    return WZMContentTypeTopBar|WZMContentTypeBottomBar;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert categories:nil];
     [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
     [UIApplication sharedApplication].applicationIconBadgeNumber = 100;
-}
-
-- (WZMContentType)contentType {
-    return WZMContentTypeTopBar|WZMContentTypeBottomBar;
 }
 
 @end
